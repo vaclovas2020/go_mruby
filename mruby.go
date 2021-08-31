1d05e9f3909ada7147884f224758175b7febdaa9
vaclovas2020 <vaclovas.lapinskis@gmail.com>
2021-08-31 18:50:22 +0000

    move c code to separate file

--- mruby.go
+++ mruby.go
@@ -3,26 +3,7 @@ package go_mruby
 //#cgo CFLAGS: -Imruby/include
 //#cgo CFLAGS: -std=c99
 //#cgo LDFLAGS: ./mruby/libmruby.a -lm
-//#include <mruby.h>
-//#include <mruby/compile.h>
-//#include <stdio.h>
-//
-//static void mruby_load_from_string(char * code)
-//{
-//  mrb_state *mrb = mrb_open();
-//  if (!mrb) { /* handle error */ }
-//  mrb_load_string(mrb, code);
-//  mrb_close(mrb);
-//}
-//
-//static void mruby_load_from_file(char * fname)
-//{
-//  FILE * file = fopen(fname, "r");
-//  mrb_state *mrb = mrb_open();
-//  if (!mrb) { /* handle error */ }
-//  mrb_load_file(mrb, file);
-//  mrb_close(mrb);
-//}
+//#include "mruby_go.c"
 import "C"
 
 func Mruby_load_from_string(code string) {
@@ -31,4 +12,12 @@ func Mruby_load_from_string(code string) {
 
 func Mruby_load_from_file(filepath string) {
 	C.mruby_load_from_file(C.CString(filepath))
+}
+
+func Mruby_open() {
+	C.mruby_open()
+}
+
+func Mruby_close() {
+	C.mruby_close()
 }
---
+++ mruby_go.c
@@ -0,0 +1,41 @@
+#include <mruby.h>
+#include <mruby/compile.h>
+#include <stdio.h>
+
+static mrb_state *mrb;
+
+static void mruby_open()
+{
+  mrb_state *mrb = mrb_open();
+}
+
+static void mruby_load_from_string(char *code)
+{
+  if (!mrb)
+  {
+    fprintf(stderr, "mrb_state is not open!");
+    return;
+  }
+  mrb_load_string(mrb, code);
+}
+
+static void mruby_load_from_file(char *fname)
+{
+  FILE *file = fopen(fname, "r");
+  if (!mrb)
+  {
+    fprintf(stderr, "mrb_state is not open!");
+    return;
+  }
+  mrb_load_file(mrb, file);
+}
+
+static void mruby_close()
+{
+  if (!mrb)
+  {
+    fprintf(stderr, "mrb_state is not open!");
+    return;
+  }
+  mrb_close(mrb);
+}
