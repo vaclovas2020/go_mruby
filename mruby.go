7339132c2c81e070773a8562a23729bbf7d07eac
vaclovas2020 <vaclovas.lapinskis@gmail.com>
2021-08-31 18:28:01 +0000

    added func Mruby_load_from_file

--- mruby.go
+++ mruby.go
@@ -5,6 +5,7 @@ package go_mruby
 //#cgo LDFLAGS: ./mruby/libmruby.a -lm
 //#include <mruby.h>
 //#include <mruby/compile.h>
+//#include <stdio.h>
 //
 //static void mruby_load_from_string(char * code)
 //{
@@ -13,8 +14,21 @@ package go_mruby
 //  mrb_load_string(mrb, code);
 //  mrb_close(mrb);
 //}
+//
+//static void mruby_load_from_file(char * fname)
+//{
+//  FILE * file = fopen(fname, "r");
+//  mrb_state *mrb = mrb_open();
+//  if (!mrb) { /* handle error */ }
+//  mrb_load_file(mrb, file);
+//  mrb_close(mrb);
+//}
 import "C"
 
 func Mruby_load_from_string(code string) {
 	C.mruby_load_from_string(C.CString(code))
+}
+
+func Mruby_load_from_file(filepath string) {
+	C.mruby_load_from_file(C.CString(filepath))
 }
